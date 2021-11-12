3a51e775b4f41156cedc8380ee98e8cdf7fbf16d
Albert <albertgaleev952@gmail.com>
2021-11-12 21:35:44 +0300

    deleted btn for copy value (dont work for ie11)

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-const copyText = document.querySelector("#id-transaction");
-if (copyText) {
-    const copyBtn = document.querySelector(".id-copy__btn");
-    copyBtn.addEventListener("click", function() {
-        navigator.clipboard.writeText(copyText.value);
-    });
-}
