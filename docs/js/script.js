ea3a4fda52ee4fdf103bbc6df9dcfe34a7e8893b
Albert <albertgaleev952@gmail.com>
2021-11-12 21:28:59 +0300

    added button for copy value(test on ie11)

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,7 @@
-console.log('test project');
+const copyText = document.querySelector("#id-transaction");
+if (copyText) {
+    const copyBtn = document.querySelector(".id-copy__btn");
+    copyBtn.addEventListener("click", function() {
+        navigator.clipboard.writeText(copyText.value);
+    });
+}
