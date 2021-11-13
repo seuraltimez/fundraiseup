df76df998d198e8d57fed580a1a9aa8ef9d877bd
Albert <albertgaleev952@gmail.com>
2021-11-13 23:30:20 +0300

    desktop version ready for chrome

--- docs/js/script.js
+++ docs/js/script.js
@@ -22,20 +22,77 @@ function initTabs() {
 
 initTabs();
 
-const ranges = document.querySelectorAll('.input-range-block');
-
-function initRange(rangeBlock) {
-  const inputRange = rangeBlock.querySelector('.behavior-item__value-range');
-  const rangeScore = rangeBlock.querySelector('.score');
-
-  rangeScore.innerHTML = inputRange.value + 'px';
-
-  inputRange.oninput = () => {
-    rangeScore.innerHTML = inputRange.value + 'px';
+// Polyfill forEach
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
   };
 }
 
-ranges.forEach((range) => {
-  initRange(range);
+const ranges = document.querySelectorAll('.input-range-block');
+
+function isOlderEdgeOrIE() {
+  return (
+    window.navigator.userAgent.indexOf('MSIE ') > -1 ||
+    !!navigator.userAgent.match(/Trident.*rv\:11\./) ||
+    window.navigator.userAgent.indexOf('Edge') > -1
+  );
+}
+
+function valueTotalRatio(value, min, max) {
+  return ((value - min) / (max - min)).toFixed(2);
+}
+
+function getLinearGradientCSS(ratio, leftColor, rightColor) {
+  return [
+    '-webkit-gradient(',
+    'linear, ',
+    'left top, ',
+    'right top, ',
+    'color-stop(' + ratio + ', ' + leftColor + '), ',
+    'color-stop(' + ratio + ', ' + rightColor + ')',
+    ')'
+  ].join('');
+}
+
+function updateRangeEl(rangeEl) {
+  const ratio = valueTotalRatio(rangeEl.value, rangeEl.min, rangeEl.max);
+  rangeEl.style.backgroundImage = getLinearGradientCSS(ratio, '#2EB670', '#DBDCDE');
+}
+
+function initRangeEl(rangeBlock) {
+  const rangeEl = rangeBlock.querySelector('.behavior-item__value-range');
+  const textEl = rangeBlock.querySelector('.score');
+
+  if (isOlderEdgeOrIE()) {
+    rangeEl.addEventListener('change', function(e) {
+      textEl.value = e.target.value + 'px';
+    });
+    rangeEl.addEventListener('input', function(e) {
+      textEl.value = e.target.value + 'px';
+    });
+  } else {
+    updateRangeEl(rangeEl);
+    rangeEl.addEventListener('input', function(e) {
+      updateRangeEl(e.target);
+      textEl.value = e.target.value + 'px';
+    });
+  }
+}
+
+ranges.forEach(function (range) {
+  initRangeEl(range);
 });
 
+const selection = document.querySelector('#currency');
+
+window.onload = function() {
+  selection.onchange = function(event){
+    const dataCurrency = event.target.options[event.target.selectedIndex].dataset.currency;
+    const labelCurrency = document.querySelector('.value-goal-result');
+    labelCurrency.innerHTML = dataCurrency;
+  };
+};
